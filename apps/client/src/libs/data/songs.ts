0f927ee80333e59a9027a5a12f7d6653cb598434
Tinnaphat Tin Somsang (Tinarskii) <tinvv@outlook.co.th>
2022-10-25 01:37:02 +0700

    Update songs.ts

--- apps/client/src/libs/data/songs.ts
+++ apps/client/src/libs/data/songs.ts
@@ -35,12 +35,14 @@ export const songs: AniSong[] = [
 	  kana: {
 			name: 'Dimension Tripper!!!!',
 			author: 'nao',
-			lyrics: 'そう今全ての次元が 結ばれる様に明日を飛び超えて まだ見えない新たな歴史を 描けるように…「Fortune favors the brave.」Dimension tripper!!!!'
+			lyrics: 
+			  'そう今全ての次元が 結ばれる様に明日を飛び超えて まだ見えない新たな歴史を 描けるように…「Fortune favors the brave.」Dimension tripper!!!!'
 		},
 		romaji: {
 			name: 'Dimension Tripper!!!!',
 			author: 'nao',
-			lyrics: '...'
+			lyrics: 
+			  'sou ima subete no jigen ga musubareru you ni ashita wo tobikoete mada mienai aratana rekishi wo egakeru youni… “Fortune favors the brave.” Dimension tripper!!!!'
 		},
 		url: 'https://youtu.be/lplZrTGkvWY&t=209&end=232'
 	}
