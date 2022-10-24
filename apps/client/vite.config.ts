1a3340a395e63377e0cb4d7c168ced413260de66
Gusb3ll <Kitpipat.0647@gmail.com>
2022-10-24 16:46:50 +0700

    feat: init

---
+++ apps/client/vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import WindiCSS from 'vite-plugin-windicss'
+import solidPlugin from 'vite-plugin-solid'
+import { createHtmlPlugin } from 'vite-plugin-html'
+
+export default defineConfig({
+	plugins: [
+		solidPlugin(),
+		WindiCSS({
+			scan: {
+				fileExtensions: ['html', 'js', 'ts', 'jsx', 'tsx']
+			}
+		}),
+		createHtmlPlugin({
+			minify: true
+		})
+	],
+	server: {
+		port: 3000
+	},
+	build: {
+		minify: true,
+		sourcemap: false,
+		target: 'esnext'
+	}
+})
