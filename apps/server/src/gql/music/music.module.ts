f374c278160852be4923e214be8ad15b5b949daa
Kitpipat Jaritwong <Kitpipat.0647@gmail.com>
2022-10-25 11:53:02 +0700

    refactor: i gave up

--- apps/server/src/gql/music/music.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common'
-
-import { MusicResolver } from './music.resolver'
-import { MusicService } from './music.service'
-
-@Module({
-  providers: [MusicResolver, MusicService]
-})
-export class MusicModule {}
